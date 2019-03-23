fd4c8c91d1f75da24d352bb6bb31b9eb9f3a2152
Joshua Manders-Jones <joshmj2@gmail.com>
2019-03-23 19:09:26 +0000

    Added basic AI node graph system and navigation

---
+++ Assets/Movement/AIpathfinding/AIagent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIagent : MonoBehaviour {
+	public float Speed;
+
+	public Rigidbody2D rb;
+	public Vector2 V;
+
+	public Node Current;
+
+	Stack<Node> Path;
+	public Node[] ToPath;
+
+	void GenPathTo(Node Destination)
+	{
+		//PlaceHolder
+	}
+
+	private void Start()
+	{
+		Path = new Stack<Node>();
+		foreach (Node i in ToPath)
+		{
+			Path.Push(i);
+		}
+	}
+
+	private void Update()
+	{
+		rb.velocity = ((Vector2)(Current.gameObject.transform.position - transform.position)).normalized * Speed;
+		V = ((Vector2)(Current.gameObject.transform.position - transform.position)).normalized * Speed;
+
+		if (Vector2.Distance(Current.gameObject.transform.position, transform.position) < 0.01f && Path.Count > 0)
+		{
+			Current = Path.Pop();
+		}
+	}
+}
---
+++ Assets/Movement/AIpathfinding/StairNode.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StairNode : Node
+{
+	public StairNode Other;
+	public float Floor;
+}
