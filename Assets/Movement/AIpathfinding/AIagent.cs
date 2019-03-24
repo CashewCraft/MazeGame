869703f3fa83adff4cc9bb3f98833448a0bc2c75
Joshua Manders-Jones <joshmj2@gmail.com>
2019-03-24 19:28:54 +0000

    Implimented A* pathfinding functionality to the AI agent, additionally changed scene to showcase

--- Assets/Movement/AIpathfinding/AIagent.cs
+++ Assets/Movement/AIpathfinding/AIagent.cs
@@ -10,24 +10,97 @@ public class AIagent : MonoBehaviour {
 
 	public Node Last;
 	public Node Current;
-
-	Stack<Node> Path;
+	private Stack<Node> Path = new Stack<Node>();
 	public Node[] ToPath;
 
 	public float Debug;
 
 	void GenPathTo(Node Destination)
 	{
-		//PlaceHolder
+		int Deadman = 0;
+
+		print("Begining pathfinding");
+
+		List<PathfindingNodeInfo> Closed = new List<PathfindingNodeInfo>();
+		List<PathfindingNodeInfo> Open = new List<PathfindingNodeInfo>
+		{
+			new PathfindingNodeInfo(0, Destination, Current, null)
+		};
+
+		PathfindingNodeInfo Target = Open[0];
+		while (Target.Represent != Destination && Deadman < 20)
+		{
+			Deadman++;
+
+			print("Begining to look for lowest cost open node");
+			float LowestCost = Open[0].Fcost+1;
+			foreach (PathfindingNodeInfo i in Open)
+			{
+				if (i.Fcost < LowestCost)
+				{
+					print("Found new lowest cost target with a value of " + i.Fcost);
+					Target = i;
+					LowestCost = i.Fcost;
+				}
+			}
+			print("Done looking for target with lowest cost");
+
+			List<Node> Connections = Target.Represent.Connections;
+			for (int Edge = 0; Edge < Connections.Count; Edge++)
+			{
+				bool FoundMatch = false;
+
+				foreach (PathfindingNodeInfo i in Closed)
+				{
+					if (i.Represent == Connections[Edge])
+					{
+						FoundMatch = true;
+						break;
+					}
+				}
+
+				if (!FoundMatch)
+				{
+					for (int i = 0; i < Open.Count; i++)
+					{
+						if (Open[i].Represent == Connections[Edge]) //if the current info is representing the current node
+						{
+							FoundMatch = true;
+							PathfindingNodeInfo NewRoute = new PathfindingNodeInfo(Target.Hcost, Destination, Connections[Edge], Target);
+							if (NewRoute.Fcost < Open[i].Fcost)
+							{
+								Open[i] = NewRoute;
+							}
+							break;
+						}
+					}
+				}
+
+				if (!FoundMatch)
+				{
+					Open.Add(new PathfindingNodeInfo(Target.Hcost, Destination, Connections[Edge], Target));
+				}
+			}
+
+			Open.Remove(Target);
+			Closed.Add(Target);
+		}
+		
+		while (Target.Previous != null)
+		{
+			Path.Push(Target.Represent);
+			Target = Target.Previous;
+		}
 	}
 
 	private void Start()
 	{
-		Path = new Stack<Node>();
-		foreach (Node i in ToPath)
-		{
-			Path.Push(i);
-		}
+		//foreach (Node i in ToPath)
+		//{
+		//	Path.Push(i);
+		//}
+		GenPathTo(Last);
+		Last = Current;
 	}
 
 	private void Update()
@@ -54,5 +127,22 @@ public class AIagent : MonoBehaviour {
 	float Percent(float x, float a, float b)
 	{
 		return (x - a) / (b - a);
+	}
+}
+
+public class PathfindingNodeInfo
+{
+	public float Fcost;
+	public float Hcost;
+	public Node Represent;
+	public PathfindingNodeInfo Previous;
+
+	public PathfindingNodeInfo(float Hops, Node Target, Node This, PathfindingNodeInfo Parent)
+	{
+		MonoBehaviour.print("Creating new info with hops of " + Hops);
+		Fcost = Hops + Vector3.Distance(This.transform.position, Target.transform.position);
+		Hcost = (Parent == null) ? 0 : Hops + Vector3.Distance(This.transform.position, Parent.Represent.transform.position);
+		Represent = This;
+		Previous = Parent;
 	}
 }
