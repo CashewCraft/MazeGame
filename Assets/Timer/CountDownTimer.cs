bc734c39197264252bc03e2a7d688485ed5bda0f
Joshua Manders-Jones <joshmj2@gmail.com>
2019-04-06 12:12:57 +0100

    Bundled timer into prefab and added text display

--- Assets/Timer/CountDownTimer.cs
+++ Assets/Timer/CountDownTimer.cs
@@ -8,6 +8,10 @@ public class CountDownTimer : MonoBehaviour {
 	public float maxTime = 5f;
 	float timeLeft;
 	public GameObject timesUpText;
+	public Text TimeDisplay;
+
+	public Color StartColour;
+	public Color EndColour;
 
 	void Start()
 	{
@@ -18,17 +22,21 @@ public class CountDownTimer : MonoBehaviour {
 
 	void Update()
 	{
-		if(timeLeft > 0)
+		if (timeLeft > 0)
 		{
 			timeLeft -= Time.deltaTime;
 			timerBar.fillAmount = timeLeft/maxTime;
+
+			TimeDisplay.text = ((timeLeft >= 600) ? "" : "0") + Mathf.FloorToInt(timeLeft / 60) + ":" + ((timeLeft % 60 > 10) ? "" : "0") + Mathf.FloorToInt(timeLeft % 60);
+			timerBar.color = Color.Lerp(EndColour, StartColour, timeLeft / maxTime);
 		}
 		else
 		{
-			{
-				timesUpText.SetActive(true);
-				Time.timeScale = 0;
-			}
+			TimeDisplay.text = "00:00";
+			timerBar.color = EndColour;
+
+			timesUpText.SetActive(true);
+			Time.timeScale = 0;
 		}
 	}
 }
