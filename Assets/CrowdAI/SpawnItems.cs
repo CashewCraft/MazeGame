a2ca6085b973dc597e8fba6307d833d9f8661425
Joshua Manders-Jones <joshmj2@gmail.com>
2019-04-06 11:42:47 +0100

    [Omar] Added Files for crowd AI and Timer

---
+++ Assets/CrowdAI/SpawnItems.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItems : MonoBehaviour {
+
+public GameObject[] Coins;
+	public Transform[] SpawnPoints;
+	public float spawnTime = 1.5f;
+
+	
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("SpawnCoins", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+	void SpawnCoins()
+	{
+		int spawnIndex = Random.Range(0, SpawnPoints.Length);
+
+		int objectIndex = Random.Range(0, Coins.Length);
+		Instantiate(Coins[objectIndex], SpawnPoints [spawnIndex].position, SpawnPoints [spawnIndex].rotation);
+	}
+}
