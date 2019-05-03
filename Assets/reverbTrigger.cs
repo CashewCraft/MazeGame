93afa71f5697737eaca99cec32bd3c6fdc5b86fe
Brennan Ceballos Peña <brennanpena@hotmail.co.uk>
2019-05-03 11:06:12 +0100

    Snapshots and transitions of reverb

---
+++ Assets/reverbTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reverbTrigger : MonoBehaviour
+{
+    public reverbControl revControl;
+    public int reverbID;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Player has passed through " + gameObject.name);
+        revControl.blendSnapshot(reverbID);
+    }
+}
